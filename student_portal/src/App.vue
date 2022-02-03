fd9a52e56499a85c6af5dce58fd765694e39f9c1
CKMolinga <ck.molinga@gmail.com>
2022-02-03 12:50:29 +0100

    Created a new vue app for the students Portal

---
+++ student_portal/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Student Portal</h1>
+    <welcome/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Welcome from './components/Welcome.vue'
+
+export default {
+  name: 'App', 
+  components: {
+    Welcome
+  },
+}
+</script>
---
+++ student_portal/src/components/Welcome.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="students">
+        <h2>Student List</h2>
+    </div>
+</template>
