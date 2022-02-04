c81074b59d8bee71655de46a33ee245fc71eadf7
CKMolinga <ck.molinga@gmail.com>
2022-02-04 01:28:40 +0100

    Created a fetch method to fetch json data from the server

--- student_portal/src/components/Welcome.vue
+++ student_portal/src/components/Welcome.vue
@@ -1,5 +1,25 @@
 <template>
     <div class="students">
         <h2>Student List</h2>
+        <div v-for="student in students" :key="student.id" class="student"></div>
+        <!-- <router-link >
+            <h2>{{ student.name }}</h2>
+        </router-link> -->
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            students: []
+        }
+    },
+     mounted() {
+            fetch('http://localhost:8080/')
+            .then((res) => res.json())
+            .then(data => this.students = data)
+            .catch(err => console.log(err.message))
+        }
+}
+</script>
